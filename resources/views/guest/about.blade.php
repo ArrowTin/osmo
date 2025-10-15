34d3dc16727e3b31448018b8a3494ac6aed4a80d
ArrowTin <tinasandrianus@gmail.com>
2025-10-15 20:38:41 +0700

    link

--- resources/views/guest/about.blade.php
+++ resources/views/guest/about.blade.php
@@ -10,15 +10,18 @@
           [
               'icon' => asset('img/help.png'),
               'title' => 'Bantuan',
+              'link' => route('instruction'),
               'desc' => 'Pelajari konsep dasar seperti aljabar, geometri, dan trigonometri dengan penjelasan visual yang mudah dimengerti.'
-          ],
-          [
+            ],
+            [
               'icon' => asset('img/game.png'),
+              'link' => route('login'),
               'title' => 'Game',
               'desc' => 'Uji kemampuanmu dengan latihan soal interaktif dan pembahasan otomatis untuk setiap jawaban.'
-          ],
-          [
+            ],
+            [
               'icon' => asset('img/leaderboard.png'),
+              'link' => route('leaderboard'),
               'title' => 'Leaderboard',
               'desc' => 'Data kemajuan belajarmu tersimpan aman. Kamu bisa belajar di mana saja tanpa kehilangan progres.'
           ],
@@ -27,7 +30,9 @@
         <div class="mx-auto w-32 h-32 rounded-full bg-indigo-100 flex items-center justify-center mb-4">
           <img src="{{ $f['icon'] }}" alt="icon" class="w-32 h-32 object-contain" />
         </div>
-        <h3 class="font-semibold px-5 py-2.5 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700">{{ $f['title'] }}</h3>
+        <a href="{{$f['link']}}">
+          <h3 class="font-semibold px-5 py-2.5 rounded-lg bg-indigo-600 text-white hover:bg-indigo-700">{{ $f['title'] }}</h3>
+        </a>
         <p class="text-sm text-gray-600 mt-1">{{ $f['desc'] }}</p>
       </div>
       @endforeach
